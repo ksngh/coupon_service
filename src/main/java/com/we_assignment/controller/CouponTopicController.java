684279279cc263267b016f911629cb83b7e30828
ksngh <chzhqk98@naver.com>
2024-12-08 14:37:33 +0900

    feat : create coupon topic (#12)

--- src/main/java/com/we_assignment/controller/CouponTopicController.java
+++ src/main/java/com/we_assignment/controller/CouponTopicController.java
@@ -1,4 +1,26 @@
 package com.we_assignment.controller;
 
+import com.we_assignment.common.CustomApiResponse;
+import com.we_assignment.common.CustomResponseMessage;
+import com.we_assignment.dto.request.CouponTopicRequestDto;
+import com.we_assignment.enums.SuccessMessage;
+import com.we_assignment.service.CouponTopicService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class CouponTopicController {
+
+    private final CouponTopicService couponTopicService;
+
+    @PostMapping()
+    public CustomApiResponse<?> createTopic(@RequestBody CouponTopicRequestDto.Create couponTopicRequestDto) {
+        couponTopicService.createCouponTopic(couponTopicRequestDto);
+        return CustomApiResponse.ok(new CustomResponseMessage("Coupon Topic" + SuccessMessage.CREATE));
+    }
 }
--- src/main/java/com/we_assignment/service/CouponTopicService.java
+++ src/main/java/com/we_assignment/service/CouponTopicService.java
@@ -1,4 +1,31 @@
 package com.we_assignment.service;
 
+import com.we_assignment.dto.request.CouponTopicRequestDto;
+import com.we_assignment.entity.CouponTopic;
+import com.we_assignment.repository.jpa.CouponTopicRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class CouponTopicService {
+
+    private final CouponTopicRepository couponTopicRepository;
+
+    @Transactional
+    public void createCouponTopic(CouponTopicRequestDto.Create couponTopicRequestDto) {
+        CouponTopic couponTopic = createDtoToCouponTopic(couponTopicRequestDto);
+        couponTopicRepository.save(couponTopic);
+    }
+
+    public CouponTopic createDtoToCouponTopic(CouponTopicRequestDto.Create couponTopicRequest) {
+        return CouponTopic.builder()
+                .id(UUID.randomUUID())
+                .name(couponTopicRequest.getName())
+                .description(couponTopicRequest.getDescription())
+                .build();
+    }
 }
