5ea4792a5e931a597cb220ba250bbf363103af8c
ksngh <chzhqk98@naver.com>
2024-12-08 18:01:39 +0900

    feat : get coupon topic (#12)

--- src/main/java/com/we_assignment/controller/CouponTopicController.java
+++ src/main/java/com/we_assignment/controller/CouponTopicController.java
@@ -3,13 +3,13 @@ package com.we_assignment.controller;
 import com.we_assignment.common.CustomApiResponse;
 import com.we_assignment.common.CustomResponseMessage;
 import com.we_assignment.dto.request.CouponTopicRequestDto;
+import com.we_assignment.dto.response.CouponTopicResponseDto;
 import com.we_assignment.enums.SuccessMessage;
 import com.we_assignment.service.CouponTopicService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,9 +18,16 @@ public class CouponTopicController {
 
     private final CouponTopicService couponTopicService;
 
-    @PostMapping()
+    @PostMapping("/coupontopics")
     public CustomApiResponse<?> createTopic(@RequestBody CouponTopicRequestDto.Create couponTopicRequestDto) {
         couponTopicService.createCouponTopic(couponTopicRequestDto);
         return CustomApiResponse.ok(new CustomResponseMessage("Coupon Topic" + SuccessMessage.CREATE));
     }
+
+    @GetMapping("/coupontopics/{coupontopicsId}")
+    public CustomApiResponse<?> getCouponTopic(@PathVariable UUID coupontopicsId) {
+        CouponTopicResponseDto couponTopicResponseDto = couponTopicService.getCouponTopicResponseDtoById(coupontopicsId);
+        return CustomApiResponse.ok(couponTopicResponseDto);
+    }
+
 }
--- src/main/java/com/we_assignment/service/CouponTopicService.java
+++ src/main/java/com/we_assignment/service/CouponTopicService.java
@@ -1,7 +1,9 @@
 package com.we_assignment.service;
 
 import com.we_assignment.dto.request.CouponTopicRequestDto;
+import com.we_assignment.dto.response.CouponTopicResponseDto;
 import com.we_assignment.entity.CouponTopic;
+import com.we_assignment.exception.coupontopic.CouponTopicNullPointerException;
 import com.we_assignment.repository.jpa.CouponTopicRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -27,5 +29,14 @@ public class CouponTopicService {
                 .name(couponTopicRequest.getName())
                 .description(couponTopicRequest.getDescription())
                 .build();
+    }
+
+    public CouponTopic getCouponTopicById(UUID couponTopicId) {
+        return couponTopicRepository.findById(couponTopicId).orElseThrow(CouponTopicNullPointerException::new);
+    }
+
+    public CouponTopicResponseDto getCouponTopicResponseDtoById(UUID couponTopicId) {
+        CouponTopic couponTopic = getCouponTopicById(couponTopicId);
+        return new CouponTopicResponseDto(couponTopic.getName(), couponTopic.getDescription());
     }
 }
