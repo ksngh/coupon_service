919b25af866220a02857c59dc2fd5280eedde876
ksngh <chzhqk98@naver.com>
2024-12-10 01:14:07 +0900

    feat : soft delete coupon & couponTopic (#23)

--- src/main/java/com/we_assignment/controller/CouponTopicController.java
+++ src/main/java/com/we_assignment/controller/CouponTopicController.java
@@ -1,4 +1,25 @@
 package com.we_assignment.controller;
 
+import com.we_assignment.common.CustomApiResponse;
+import com.we_assignment.common.CustomResponseMessage;
+import com.we_assignment.enums.SuccessMessage;
+import com.we_assignment.service.CouponTopicService;
+import com.we_assignment.service.coupon.CouponService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class CouponTopicController {
+
+    private final CouponTopicService couponTopicService;
+
+    @DeleteMapping("/coupontopics/{couponTopicId}")
+    public CustomApiResponse<?> deleteCouponTopic(@PathVariable UUID couponTopicId) {
+        couponTopicService.softDelete(couponTopicId);
+        return CustomApiResponse.ok(new CustomResponseMessage("Coupon " + SuccessMessage.DELETE));
+    }
 }
--- src/main/java/com/we_assignment/service/CouponTopicService.java
+++ src/main/java/com/we_assignment/service/CouponTopicService.java
@@ -1,4 +1,33 @@
 package com.we_assignment.service;
 
+import com.we_assignment.entity.CouponTopic;
+import com.we_assignment.entity.Timestamped;
+import com.we_assignment.exception.coupontopic.CouponTopicNullPointerException;
+import com.we_assignment.repository.jpa.CouponRepository;
+import com.we_assignment.repository.jpa.CouponTopicRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class CouponTopicService {
+
+    private final CouponTopicRepository couponTopicRepository;
+    private final CouponRepository couponRepository;
+
+    @Transactional
+    public void softDelete(UUID couponTopicId) {
+        CouponTopic couponTopic = couponTopicRepository.findById(couponTopicId).orElseThrow(CouponTopicNullPointerException::new);
+        couponTopic.delete();
+        couponSoftDelete(couponTopic);
+    }
+
+    public void couponSoftDelete(CouponTopic couponTopic) {
+        couponRepository.findAllByCouponTopic(couponTopic).forEach(Timestamped::delete);
+    }
+
+
 }
